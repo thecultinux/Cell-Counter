7978b439401aae21741bebcc42a999d3afcfd1d0
aziz <yahmadi99aziz@gmail.com>
2022-03-07 17:16:15 +0100

    lecture pgm

--- test/test_pgm_read.c
+++ test/test_pgm_read.c
@@ -4,7 +4,7 @@ int main(){
   image fichier;
   fichier=readPgm("image.pgm");
   if (fichier.width == 0) {
-    printf("error: file not found");
+    printf("error: file not found \n");
     return 1 ;
   }
   printf("la longueur:%d , la largeur:%d , la profondeur:%d \n",fichier.width, fichier.height , fichier.depth);
