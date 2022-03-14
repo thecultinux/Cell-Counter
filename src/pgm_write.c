5f17fbe17186b5799eed9a9c91a0f94940280c48
mahe <youwan.mahe@phelma.inp-grenoble.fr>
2022-03-14 15:01:31 +0100

    ecriture de fichier fonctionnelle

---
+++ src/pgm_write.c
@@ -0,0 +1,18 @@
+#include "pgm.h"
+
+int i,j;
+int pixel=0;
+
+int writePgm( image fichier ) {
+  FILE* f = NULL;
+  f = fopen("copy.pgm","wb");
+  if (f==NULL) {
+    return -1;
+  }
+
+  rewind(f);
+  fprintf(f, "P5\n%d %d\n%d\n",fichier.width, fichier.height, fichier.depth);
+  fwrite(fichier.data[0],sizeof(char),fichier.height*fichier.width,f);
+  fclose(f);
+  return 0;
+}
---
+++ test/test_pgm_write.c
@@ -0,0 +1,23 @@
+#include "pgm.h"
+
+int main(){
+  int i,j;
+
+  image fichier;
+  fichier=readPgm("image.pgm");
+  if (fichier.width == 0) {
+    printf("error: file not found \n");
+    return 1 ;
+  }
+  printf("longueur:%d ||  largeur:%d ||  profondeur:%d \n",fichier.width, fichier.height , fichier.depth);
+  for (i = 0; i < fichier.height; i++) {
+    for (j = 0; j < fichier.width; j++) {
+      printf("%d ", fichier.data[i][j]);
+    }
+    printf("\n");
+  }
+  if (!writePgm(fichier)) {
+    printf("Succesfully copied pgm file\n");
+  }
+  return 0;
+}
