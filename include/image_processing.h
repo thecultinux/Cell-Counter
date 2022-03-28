044861e5d80e3cb7f8adf055abe423a852a89106
aziz <yahmadi99aziz@gmail.com>
2022-03-28 15:47:38 +0200

    fonctions suppression des cellules aux bords et inversion (image edge pas encore générée)

--- include/image_processing.h
+++ include/image_processing.h
@@ -1,5 +1,7 @@
 #ifndef IMAGE_PROCESSING_H
 #define IMAGE_PROCESSING_H
 
+image edge_cells_removal(image fichier , image edge);
+int reverse (image fichier);
 
 #endif
--- src/image_processing.c
+++ src/image_processing.c
@@ -1,1 +1,28 @@
 #include "library.h"
+
+
+image edge_cells_removal(image fichier , image edge){
+  image reconst = fichier;
+  image result = fichier;
+  reconst=allocate(reconst);
+  result=allocate(result);
+  reconst = reconstruct(fichier,edge);
+  result = XOR(reconst, fichier);
+  return result;
+}
+
+int reverse (image fichier){
+  int i,j;
+  for (i=0 ; i<fichier.height ; i++)
+    for (j=0 ; j<fichier.width ; j++) {
+      if (fichier.data[i][j]==0) fichier.data[i][j]=255;
+      if (fichier.data[i][j]==255) fichier.data[i][j]=0;
+    }
+  return 1;
+}
+
+/*image hole_plugging(image fichier , image edge){
+  image result = fichier;
+  result=allocate(result);
+  reverse(fichier)
+}*/
