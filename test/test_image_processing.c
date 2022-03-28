d62b60ce07f286c15d9a3dfca4422d26c2826d13
aziz <yahmadi99aziz@gmail.com>
2022-03-28 16:45:06 +0200

    ajout d'une fonction de test du bouchage des trous

--- test/test_image_processing.c
+++ test/test_image_processing.c
@@ -9,15 +9,27 @@ int main(int argc, char** argv){
 
   if (!strcmp( argv[1], "b" ) ||
       !strcmp( argv[1], "B" ) ) {
-    image fichier;
-    fichier=readPgm(argv[2]);
-    fichier=erode(fichier);
-    writePgm(fichier,"border_cells_removed.pgm");
-    freeImage(fichier);
-    printf("Border cells removed\n");
-    return 0;
+        image fichier;
+        fichier=readPgm(argv[2]);
+        fichier=delete_edge_cell(fichier);
+        writePgm(fichier,"border_cells_removed.pgm");
+        freeImage(fichier);
+        printf("Border cells removed\n");
+        return 0;
   }
+
+  if (!strcmp( argv[1], "h" ) ||
+      !strcmp( argv[1], "H" ) ) {
+        image fichier;
+        fichier=readPgm(argv[2]);
+        fichier=hole_plugging(fichier);
+        writePgm(fichier,"holes_plugged.pgm");
+        freeImage(fichier);
+        printf("Holes plugged\n");
+        return 0;
+  }
+
   else{
-      printf("Nothing to be done\n");
-    }
+    printf("Nothing to be done\n");
   }
+}
--- test/test_morphology.c
+++ test/test_morphology.c
@@ -9,39 +9,39 @@ int main(int argc, char** argv){
 
   if (!strcmp( argv[1], "er" ) ||
       !strcmp( argv[1], "ER" ) ) {
-    image fichier;
-    fichier=readPgm(argv[2]);
-    fichier=erode(fichier);
-    writePgm(fichier,"eroded.pgm");
-    freeImage(fichier);
-    printf("Erosion done\n");
-    return 0;
+        image fichier;
+        fichier=readPgm(argv[2]);
+        fichier=erode(fichier);
+        writePgm(fichier,"eroded.pgm");
+        freeImage(fichier);
+        printf("Erosion done\n");
+        return 0;
   }
 
-if (!strcmp( argv[1], "ex" ) ||
-    !strcmp( argv[1], "EX" ) ) {
-      image fichier;
-      fichier=readPgm(argv[2]);
-      fichier=expanse(fichier);
-      writePgm(fichier,"expansed.pgm");
-      freeImage(fichier);
-      printf("Expansion done\n");
-      return 0;
+  if (!strcmp( argv[1], "ex" ) ||
+      !strcmp( argv[1], "EX" ) ) {
+        image fichier;
+        fichier=readPgm(argv[2]);
+        fichier=expanse(fichier);
+        writePgm(fichier,"expansed.pgm");
+        freeImage(fichier);
+        printf("Expansion done\n");
+        return 0;
   }
 
 
-if (!strcmp( argv[1], "re" ) ||
-    !strcmp( argv[1], "re" ) ) {
-      image fichier,seed;
-      fichier=readPgm(argv[2]);
-      seed=readPgm(argv[3]);
-      fichier=reconstruct(fichier,seed);
-      writePgm(fichier,"reconstructed.pgm");
-      freeImage(fichier);
-      printf("Reconstruction done\n");
-      return 0;
-    }
-else{
+  if (!strcmp( argv[1], "re" ) ||
+      !strcmp( argv[1], "re" ) ) {
+        image fichier,seed;
+        fichier=readPgm(argv[2]);
+        seed=readPgm(argv[3]);
+        fichier=reconstruct(fichier,seed);
+        writePgm(fichier,"reconstructed.pgm");
+        freeImage(fichier);
+        printf("Reconstruction done\n");
+        return 0;
+  }
+  else{
     printf("Nothing to be done\n");
   }
 }
