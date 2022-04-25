7843cee9ca432b76e494daf72c53142a62e2e113
mahe <youwan.mahe@phelma.inp-grenoble.fr>
2022-04-25 14:17:49 +0200

    correction erreur de frappe

--- src/counter.c
+++ src/counter.c
@@ -3,7 +3,7 @@
 int DFS(image fichier,int i, int j, int** visited){
   if (visited[i][j]==0 && fichier.data[i][j]==255) {
     visited[i][j]=1; //marque le pixel comme visité
-    DFS(fichier,i-i,j,visited);
+    DFS(fichier,i-1,j,visited);
     DFS(fichier,i+1,j,visited);
     DFS(fichier,i,j-1,visited);
     DFS(fichier,i,j+1,visited);
